d1e94cc9ea761700e5877adc010cacb5ca5ac01e
harrietfiagbor <harrietfiagbor@gmail.com>
2025-02-07 12:55:13 +0000

    add device to config

--- utils.py
+++ utils.py
@@ -1,4 +1,6 @@
 from dataclasses import dataclass
+import torch as t
+
 @dataclass
 class Config:
     d_model: int = 768
@@ -11,3 +13,4 @@ class Config:
     d_mlp: int = 3072
     n_heads: int = 12
     n_layers: int = 12
+    device = t.device("mps" if t.backends.mps.is_available() else "cuda" if t.cuda.is_available() else "cpu")
