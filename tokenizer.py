fccf211b7ebfdb8ba46fc92fc7756a42c7a4ea9b
harrietfiagbor <harrietfiagbor@gmail.com>
2025-02-07 12:36:51 +0000

    add tokenizer file

---
+++ tokenizer.py
@@ -0,0 +1,8 @@
+from transformer_lens import HookedTransformer
+
+reference_gpt2 = HookedTransformer.from_pretrained(
+    "gpt2-small",
+    fold_ln=False,
+    center_unembed=False,
+    center_writing_weights=False,  # you'll learn about these arguments later!
+)
