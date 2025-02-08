70742687120dc28cf7cb141707e6984f3beced30
harrietfiagbor <harrietfiagbor@gmail.com>
2025-02-08 18:02:10 +0000

    implement Unembed

--- demo_transformer.py
+++ demo_transformer.py
@@ -146,6 +146,22 @@ class TransformerBlock(nn.Module):
         return resid_post
 
 
+class Unembed(nn.Module):
+    def __init__(self, cfg):
+        super().__init__()
+        self.cfg = cfg
+        self.W_U = nn.Parameter(t.empty((cfg.d_model, cfg.d_vocab)))
+        nn.init.normal_(self.W_U, std=self.cfg.init_range)
+        self.b_U = nn.Parameter(t.zeros((cfg.d_vocab), requires_grad=False))
+
+    def forward(
+        self, normalized_resid_final: Float[Tensor, "batch position d_model"]
+    ) -> Float[Tensor, "batch position d_vocab"]:
+        return (
+            einops.einsum([normalized_resid_final, self.W_U], "batch position d_model, d_model d_vocab -> batch position d_vocab")
+        ) + self. b_U
+    
+
 
 
     
